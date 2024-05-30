7a468c9db66fe019de8fa9c332b58f2651cb44b8
yukimiyake0607 <yukimiyake38@gmail.com>
2024-05-30 23:13:18 +0900

    update: CurrentLocationクラスをファーストページに設定

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator_sample/current_location.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const MaterialApp(
+    home: CurrentLocation(),
+  ));
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
-  }
-}
