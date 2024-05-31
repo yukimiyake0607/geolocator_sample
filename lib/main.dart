1f82e16b81a8bcb806314322fa4ba8f0d42aa236
yukimiyake0607 <yukimiyake38@gmail.com>
2024-05-31 15:12:43 +0900

    refactor: runAppをMyAppに変更
    refs #6

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:geolocator_sample/current_location.dart';
+import 'package:geolocator_sample/first_page.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    home: CurrentLocation(),
-  ));
+  runApp(const MyApp());
 }
 
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: const FirstPage(),
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+    );
+  }
+}
