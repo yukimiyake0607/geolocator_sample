fea16a7d1dee7ab7cc85099f4aaa99d468125706
yukimiyake0607 <yukimiyake38@gmail.com>
2024-05-31 16:07:42 +0900

    add: CurrentTimeクラスの中身を作成
    refs #12 現在時刻を表示するため

---
+++ lib/current_time.dart
@@ -0,0 +1,51 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+class CurrentTime extends StatefulWidget {
+  const CurrentTime({Key? key}) : super(key: key);
+
+  @override
+  State<CurrentTime> createState() => _CurrentTimeState();
+}
+
+class _CurrentTimeState extends State<CurrentTime> {
+  late String _timeString;
+  late Timer _timer;
+
+  String formatDateTime(DateTime dateTime) {
+    return "${dateTime.hour.toString().padLeft(2, '0')}:${dateTime.minute.toString().padLeft(2, '0')}:${dateTime.second.toString().padLeft(2, '0')}";
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _timeString = formatDateTime(DateTime.now());
+    _timer = Timer.periodic(const Duration(seconds: 1), (Timer timer) {
+      _getTime();
+    });
+  }
+
+  void _getTime() {
+    final DateTime now = DateTime.now();
+    final String formattedDateTime = formatDateTime(now);
+    setState(() {
+      _timeString = formattedDateTime;
+    });
+  }
+
+  @override
+  void dispose() {
+    _timer.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      _timeString,
+      style: const TextStyle(fontSize: 48),
+    );
+  }
+}
