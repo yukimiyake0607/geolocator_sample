c09ff997fc9a91413cb42eee3e2e42c18bfe622c
yukimiyake0607 <yukimiyake38@gmail.com>
2024-05-31 15:34:14 +0900

    remove: のちに仕様追加するまで保留
    res #10

--- lib/current_location.dart
+++ lib/current_location.dart
@@ -1,15 +1,1 @@
 import 'package:flutter/material.dart';
-
-class CurrentLocation extends StatefulWidget {
-  const CurrentLocation({super.key});
-
-  @override
-  State<CurrentLocation> createState() => _CurrentLocationState();
-}
-
-class _CurrentLocationState extends State<CurrentLocation> {
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
-  }
-}
