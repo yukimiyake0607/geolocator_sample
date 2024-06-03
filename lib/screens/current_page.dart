858e2f8469a55a47cc988194828399d941254a23
yukimiyake0607 <yukimiyake38@gmail.com>
2024-06-03 17:25:39 +0900

    add: レイアウト追加
    refs 表示する天候情報のレイアウトを追加

--- lib/screens/current_page.dart
+++ lib/screens/current_page.dart
@@ -12,146 +12,127 @@ class CurrentPage extends StatefulWidget {
 class _CurrentPageState extends State<CurrentPage> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        const Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Expanded(
-              child: Column(
-                children: [
-                  Padding(
-                    padding: EdgeInsets.all(16.0),
-                    child: Column(
-                      children: [
-                        Text(
-                          '快晴',
-                          style: TextStyle(
-                              fontSize: 18, fontWeight: FontWeight.bold),
-                        ),
-                        Padding(
-                          padding: EdgeInsets.all(16.0),
-                          child: Icon(
-                            WeatherIcons.day_sunny,
-                            size: 90,
-                            color: Colors.yellow,
+    return Padding(
+      padding: const EdgeInsets.only(left: 8.0, right: 8.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Expanded(
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.all(16.0),
+                      child: Column(
+                        children: [
+                          Text(
+                            '快晴',
+                            style: TextStyle(
+                                fontSize: 18, fontWeight: FontWeight.bold),
                           ),
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            CurrentTime(),
-                            Text(
-                              '時点',
-                              style: TextStyle(fontSize: 16),
+                          Padding(
+                            padding: EdgeInsets.all(16.0),
+                            child: Icon(
+                              WeatherIcons.day_sunny,
+                              size: 90,
+                              color: Colors.yellow,
                             ),
-                          ],
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              CurrentTime(),
+                              Text(
+                                '時点',
+                                style: TextStyle(fontSize: 16),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Expanded(
+                child: Column(
+                  children: [
+                    Text(
+                      '16℃',
+                      style: TextStyle(fontSize: 50),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text('最高:20℃'),
+                        SizedBox(
+                          width: 10,
                         ),
+                        Text('最低:10℃'),
                       ],
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-            Expanded(
-              child: Column(
-                children: [
-                  Text(
-                    '16℃',
-                    style: TextStyle(fontSize: 50),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text('最高:20℃'),
-                      SizedBox(
-                        width: 10,
-                      ),
-                      Text('最低:10℃'),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
-        Expanded(
+            ],
+          ),
+          Expanded(
             child: GridView.count(
-          crossAxisCount: 2,
-          crossAxisSpacing: 10,
-          mainAxisSpacing: 10,
-          children: [
-            Container(
-              padding: EdgeInsets.all(16.0),
-              decoration: BoxDecoration(
-                color: Colors.grey.withOpacity(0.7),
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: const Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    '湿度',
-                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                  ),
-                  Text(
-                    '90%',
-                    style: TextStyle(fontSize: 40),
-                  ),
-                  Spacer(),
-                  Text('湿度は高めです'),
-                ],
-              ),
+              crossAxisCount: 2,
+              crossAxisSpacing: 10,
+              mainAxisSpacing: 10,
+              children: [
+                BuildWeatherInfo(
+                    title: '湿度',
+                    value: 90,
+                    description: '湿度は高めです',
+                    parameter: '%'),
+                BuildWeatherInfo(
+                    title: '体感温度',
+                    value: 18,
+                    description: '半袖で過ごせます',
+                    parameter: '℃'),
+                BuildWeatherInfo(
+                    title: '風速',
+                    value: 5,
+                    description: '時折少し強い風が吹きます',
+                    parameter: 'm/s'),
+                BuildWeatherInfo(
+                  title: '風向き',
+                ),
+                BuildWeatherInfo(
+                  title: '日の出',
+                  value: 6,
+                  parameter: '時',
+                ),
+                BuildWeatherInfo(
+                  title: '日の入',
+                  value: 17,
+                  parameter: '時',
+                ),
+              ],
             ),
-            Container(
-              decoration: BoxDecoration(
-                color: Colors.grey,
-                borderRadius: BorderRadius.circular(10),
-              ),
-            ),
-            Container(
-              decoration: BoxDecoration(
-                color: Colors.grey,
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: const Column(),
-            ),
-            Container(
-              decoration: BoxDecoration(
-                color: Colors.grey,
-                borderRadius: BorderRadius.circular(10),
-              ),
-            ),
-          ],
-        )),
-        Row(
-          children: [
-            Text('湿度'),
-            Text('風速'),
-            Text('風向き'),
-            Text('日の出'),
-            Text('日の入り'),
-            Text('体感温度'),
-          ],
-        ),
-      ],
+          ),
+        ],
+      ),
     );
   }
 }
 
 class BuildWeatherInfo extends StatefulWidget {
-  String title;
-  int value;
-  String description;
-  String parameter;
+  String? title;
+  int? value;
+  String? description;
+  String? parameter;
 
   BuildWeatherInfo({
     Key? key,
-    required this.title,
-    required this.value,
-    required this.description,
-    required this.parameter,
+    this.title,
+    this.value,
+    this.description,
+    this.parameter,
   }) : super(key: key);
 
   @override
@@ -171,7 +152,7 @@ class _BuildWeatherInfoState extends State<BuildWeatherInfo> {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Text(
-            widget.title,
+            widget.title ?? '',
             style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
           ),
           Row(
@@ -180,11 +161,11 @@ class _BuildWeatherInfoState extends State<BuildWeatherInfo> {
                 widget.value.toString(),
                 style: TextStyle(fontSize: 40),
               ),
-              Text(widget.parameter),
+              Text(widget.parameter ?? ''),
             ],
           ),
           Spacer(),
-          Text(widget.description),
+          Text(widget.description ?? ''),
         ],
       ),
     );
