781518940a65b12458a50f669c72ce59ae10ce4e
yukimiyake0607 <yukimiyake38@gmail.com>
2024-06-04 18:58:37 +0900

    add: WeatherModelクラスを作成
    refs 位置情報をhttpでAPI通信し、現在地の天気情報を返り値とする

---
+++ lib/services/weather.dart
@@ -0,0 +1,15 @@
+import 'package:geolocator_sample/services/current_location.dart';
+import 'package:geolocator_sample/services/network_helper.dart';
+
+class WeatherModel {
+  Future<dynamic> getLocationWeather() async {
+    CurrentLocation currentLocation = CurrentLocation();
+    await currentLocation.getLocation();
+
+    NetworkHelper networkHelper = NetworkHelper(
+        'https://api.openweathermap.org/data/2.5/weather?lat=${currentLocation.latitude}&lon=${currentLocation.longitude}&appid=faca477592cf21c0f3627e68fc1fd1b0');
+
+    dynamic weatherData = await networkHelper.getData();
+    return weatherData;
+  }
+}
