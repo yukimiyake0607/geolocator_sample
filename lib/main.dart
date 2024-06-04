c4062ef8b6b3d61cb42904fb8a17becc24b9f5b3
yukimiyake0607 <yukimiyake38@gmail.com>
2024-06-04 19:02:31 +0900

    add: runAppをLoadingPageクラスに変更

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:geolocator_sample/screens/first_page.dart';
+import 'package:geolocator_sample/screens/loading_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: FirstPage(),
+      home: LoadingPage(),
       theme: ThemeData(
         primarySwatch: Colors.blue,
         textTheme: const TextTheme(
