bacd5b50f6f794b22a6d8d2bc116f5cef98b7247
yukimiyake0607 <yukimiyake38@gmail.com>
2024-06-05 15:43:49 +0900

    update: apiKeyを定数に、nameで取得できる都市名を日本語に設定
    refs 日本向けのアプリのため英語ではわかりづらい

--- lib/services/weather.dart
+++ lib/services/weather.dart
@@ -1,13 +1,15 @@
 import 'package:geolocator_sample/services/current_location.dart';
 import 'package:geolocator_sample/services/network_helper.dart';
 
+String apiKey = 'faca477592cf21c0f3627e68fc1fd1b0';
+
 class WeatherModel {
   Future<dynamic> getLocationWeather() async {
     CurrentLocation currentLocation = CurrentLocation();
     await currentLocation.getLocation();
 
     NetworkHelper networkHelper = NetworkHelper(
-        'https://api.openweathermap.org/data/2.5/weather?lat=${currentLocation.latitude}&lon=${currentLocation.longitude}&appid=faca477592cf21c0f3627e68fc1fd1b0');
+        'https://api.openweathermap.org/data/2.5/weather?lat=${currentLocation.latitude}&lon=${currentLocation.longitude}&appid=$apiKey&units=metric&lang=ja');
 
     dynamic weatherData = await networkHelper.getData();
     return weatherData;
