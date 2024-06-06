f24d1565899016c1eb66cdfc7684eb85a3d52d94
yukimiyake0607 <yukimiyake38@gmail.com>
2024-06-06 16:13:42 +0900

    style: テキストに句点追加

--- lib/services/weather.dart
+++ lib/services/weather.dart
@@ -105,19 +105,19 @@ class WeatherModel {
 
   String sensitibleTempertureText(double sensitibleTemperture) {
     if (sensitibleTemperture >= 30) {
-      return '半袖で過ごせます';
+      return '半袖で過ごせます。';
     } else if (sensitibleTemperture >= 25) {
-      return '半袖シャツで過ごせます';
+      return '半袖シャツで過ごせます。';
     } else if (sensitibleTemperture < 25) {
-      return '長袖シャツで過ごせます';
+      return '長袖シャツで過ごせます。';
     } else if (sensitibleTemperture < 20) {
-      return '薄手のカーディガンを持ち歩きましょう';
+      return '薄手のカーディガンを持ち歩きましょう。';
     } else if (sensitibleTemperture < 15) {
-      return 'セーターで過ごせます';
+      return 'セーターで過ごせます。';
     } else if (sensitibleTemperture < 12) {
-      return 'トレンチコートを羽織りましょう';
+      return 'トレンチコートを羽織りましょう。';
     } else {
-      return '冬用アウターを羽織りましょう';
+      return '冬用アウターを羽織りましょう。';
     }
   }
 }
